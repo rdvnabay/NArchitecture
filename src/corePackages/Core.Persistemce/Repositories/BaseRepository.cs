e5871b2748e2523612520851b2b5e992c3d5905b
Ridvan <ridvanabay@gmail.com>
2022-08-23 19:52:21 +0300

    Core Repository: add AnyAsync and AddAsync method

--- src/corePackages/Core.Persistemce/Repositories/BaseRepository.cs
+++ src/corePackages/Core.Persistemce/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 namespace Core.Persistemce.Repositories;
 
 public class BaseRepository<TContext, TEntity> : IAsyncRepository<TEntity>
@@ -10,4 +11,22 @@ public class BaseRepository<TContext, TEntity> : IAsyncRepository<TEntity>
     {
         _context = context;
     }
+
+    //Add-Update-Delete
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        _context.Entry(entity).State = EntityState.Added;
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    //Get
+    public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> query)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Query
+    public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> query) =>
+        await _context.Set<TEntity>().AnyAsync(query);
 }
--- src/corePackages/Core.Persistemce/Repositories/IAsyncRepository.cs
+++ src/corePackages/Core.Persistemce/Repositories/IAsyncRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace Core.Persistemce.Repositories;
+﻿using System.Linq.Expressions;
+namespace Core.Persistemce.Repositories;
 
 public interface IAsyncRepository<T> where T : class
 {
+    Task<T> GetAsync(Expression<Func<T, bool>> query);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> query);
+    Task<T> AddAsync(T entity);
 }
